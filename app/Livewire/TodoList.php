cb743b45e4d79797bb90e96a10299e395d0d5a40
ruko19 <rukobaruna23@gmail.com>
2023-09-17 19:45:46 -0500

    pagination

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -4,28 +4,36 @@ namespace App\Livewire;
 
 use App\Models\Todo;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+
+
+    use WithPagination;
 
     public $name;
 
     public function create()
     {
 
+        // validation
         $validated = $this->validate([
             'name' => 'required|min:3'
         ]);
 
+        // create todo
         Todo::create($validated);
+        // reset Input
         $this->reset('name');
 
+        // message success
         session()->flash('success', 'Created');
     }
     public function render()
     {
+        $todos = Todo::latest()->paginate(5);
 
-
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', compact('todos'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App template</title>
-
+    @livewireStyles
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -31,6 +31,8 @@
 
     </div>
 
+
+    @livewireScripts
 </body>
 
 </html>
